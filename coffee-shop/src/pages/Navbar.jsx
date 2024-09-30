7b0a39e29b04a979371e62ae3a58468bd0e6c017
jenisha987 <jensastha987@gmail.com>
2024-09-30 17:26:51 +0545

    styled navbar

--- coffee-shop/src/pages/Navbar.jsx
+++ coffee-shop/src/pages/Navbar.jsx
@@ -1,25 +1,40 @@
 import React from 'react'
 import { Link, NavLink } from "react-router-dom";
 import { navlinks } from '../Data/Data';
+import { SiCoffeescript } from "react-icons/si";
+import { CiSearch } from "react-icons/ci";
+import { PiShoppingCartThin } from "react-icons/pi";
+import { MdMenu } from "react-icons/md";
+import { RxCross2 } from "react-icons/rx";
 
 const Navbar = () => {
   return (
-    <div>
-      <Link to="/">#Coffee</Link>
-      <nav>
-        <ul>
+    <nav>
+      <div className="container nav-container">
+        <Link to="/" className='logo'>
+          <SiCoffeescript />
+          <p>Coffee Cove</p>
+        </Link>
+        <ul className='nav-links'>
           {
             navlinks.map(({name, path}, index) => {
               return (
-                <Link to={path}>
-                  <li key={index}>{name}</li>
-                </Link>
+                <li key={index}>
+                  <NavLink to={path}>{name}</NavLink>
+                </li>
               )
             })
           }
         </ul>
-      </nav>
-    </div>
+
+        <div className="nav-right">
+          <CiSearch />
+          <PiShoppingCartThin />
+          <MdMenu />
+          <RxCross2 />
+        </div>
+      </div>
+    </nav>
   )
 }
 
