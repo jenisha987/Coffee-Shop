fa82e00b8a884fab7e3099f20209213241313c89
jenisha987 <jensastha987@gmail.com>
2024-10-01 18:50:57 +0545

    added react-carousel and some styling

--- coffee-shop/src/components/Home.jsx
+++ coffee-shop/src/components/Home.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import home from "../assets/images/Home.png";
+import home1 from "../assets/images/Home1.png";
+import { Link } from 'react-router-dom';
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export const Home = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+  };
   return (
-    <div className='container'>
-      <img src={home} alt='home' className='home-img' />
+    <div className='home'>
+    <Slider {...settings}>
+      <div>
+        <img src={home} alt="home" className="home-img" />
+        <button className='shop-btn'>Shop Now</button>
+      </div>
+      <div>
+        <img src={home1} alt="home1" className="home-img" />
+        <button className='shop-button'>Shop Now</button>
+      </div>
+    </Slider>
     </div>
   )
 }
