f50970e61bfc476aceee686dc63acce9bdc29cab
jenisha987 <jensastha987@gmail.com>
2024-10-04 11:48:05 +0545

    home-tag-section

--- coffee-shop/src/components/Products.jsx
+++ coffee-shop/src/components/Products.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { products_list } from '../Data/Products';
+import coffee_cup from "../assets/images/coffee-cup.png";
 
 const Products = () => {
 
@@ -23,21 +24,32 @@ const Products = () => {
 
   return (
     <div className='home-product'>
-        <div className='home-product-p'>
-            <p className='home-product-tag'>best seller</p>
-            <p className='home-product-tagline'>best seller product this week!</p>
+      <div className='home-product-p'>
+          <p className='home-product-tag'>best seller</p>
+          <p className='home-product-tagline'>best seller product this week!</p>
+      </div>
+      <div className='home-product-list'>
+          {
+            randomProducts.map(product_list => (
+              <div key={product_list.id} className='product-item'>
+                <img src={product_list.image} alt='product' className='home-product-img' />
+                <p>{product_list.title}</p>
+                <p>Rs.{product_list.price}</p>
+              </div>
+            ))
+          }
+      </div>
+      
+      <div className='home-tag'>
+        <div className='home-tag-section'>
+          <p className='home-tag-p'>special pure coffee</p>
+          <p className='home-tag-p-p'>When you've got your health, you got everything</p>
+          <button className='home-tag-button'>shop now</button>
         </div>
-        <div className='home-product-list'>
-            {
-              randomProducts.map(product_list => (
-                <div key={product_list.id} className='product-item'>
-                  <img src={product_list.image} alt='product' className='home-product-img' />
-                  <p>{product_list.title}</p>
-                  <p>Rs.{product_list.price}</p>
-                </div>
-              ))
-            }
+        <div>
+          <img src={coffee_cup} alt='coffee-cup' className='home-tag-img' />
         </div>
+      </div>
     </div>
   )
 }
