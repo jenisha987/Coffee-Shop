089ce83885d4fa8dfa0c5eeb30a6f6be6a6e6c8e
jenisha987 <jensastha987@gmail.com>
2024-10-04 14:28:44 +0545

    Footer section

--- coffee-shop/src/App.jsx
+++ coffee-shop/src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
       <Route path='/' element={<Home />} />
       <Route path='*' element={<Error />} />
     </Routes>
-    {/* <Footer /> */}
+    <Footer />
     </BrowserRouter>
   )
 }
--- coffee-shop/src/pages/Footer.jsx
+++ coffee-shop/src/pages/Footer.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React from 'react';
+import { SiCoffeescript } from "react-icons/si";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <hr className='footer-hr' />
+      <div className='footer'>
+        <div className='footer-section1'>
+          <div className='footer-section1-logo'>
+            <SiCoffeescript size={30} className='footer-logo' />
+            <p className='footer-header'>coffee cove</p>
+          </div>
+          <p>Subscribe our newsletter and get discount 30% off</p>
+        </div>
+        <div>
+          <p>customer care</p>
+          <ul>
+            <li>about us</li>
+            <li>privacy policy</li>
+            <li>terms & conditions</li>
+            <li>products return</li>
+            <li>wholesale privacy</li>
+          </ul>
+        </div>
+        <div>
+          <p>quick shop</p>
+          <ul>
+            <li>pagination</li>
+            <li>terms & conditions</li>
+            <li>contact</li>
+            <li>accessories</li>
+            <li>home page</li>
+          </ul>
+        </div>
+        <div>
+          <p>company</p>
+          <ul>
+            <li>menu items</li>
+            <li>help center</li>
+            <li>address store</li>
+            <li>privacy policy</li>
+            <li>home page</li>
+          </ul>
+        </div>
+      </div>
+    </footer>
   )
 }
 
