29dc7d9fbc8c51741d182ed7b63341fc88e16ca1
jenisha987 <jensastha987@gmail.com>
2024-10-06 18:05:50 +0545

    implemented + and - to the item quantity

--- coffee-shop/src/components/Cart.jsx
+++ coffee-shop/src/components/Cart.jsx
@@ -1,28 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Cart = ({ cart }) => {
+
+    const [ carts, setCarts ] = useState([]);
+
+    useEffect(() => {
+        setCarts(cart);
+    }, [cart])
+
   return (
     <div className="cart-page">
       <h2>Your Cart</h2>
       {
-        cart.map((cartItem, cartIndex) => {
+        carts?.map((cartItem, cartIndex) => {
             return (
                 <div key={cartIndex}>
                     <img src={cartItem.image} alt='cartItem' width={200} />
                     <p>{cartItem.title}</p>
-                    <button>-</button>
+                    <button onClick={() => {
+                        const _carts = carts.map((item, index) => {
+                            return cartIndex === index ? { ...item, quantity: item.quantity - 1 } : item
+                        })
+                        setCarts(_carts)
+                    }}>-</button>
                     <p>{cartItem.quantity}</p>
-                    <button>+</button>
-                    <p>Rs.{cartItem.price}</p>
+                    <button onClick={() => {
+                        const _carts = carts.map((item, index) => {
+                            return cartIndex === index ? { ...item, quantity: item.quantity + 1 } : item
+                        })
+                        setCarts(_carts)
+                    }}>+</button>
+                    <p>Rs.{cartItem.price * cartItem.quantity}</p>
                 </div>
             )
         })
       }
 
-      <p>Total Amount:</p>
+      <p>Total Amount: <span></span>
       {
-        cart.map(item => item.price).reduce((total, value) => total + value, 0)
+        cart.map(item => item.price + item.quantity).reduce((total, value) => total + value, 0)
       }
+      </p>
     </div>
   );
 };
