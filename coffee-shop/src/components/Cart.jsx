fc9937913fddb91a828f75380ace8c3718795f8b
jenisha987 <jensastha987@gmail.com>
2024-10-07 16:17:02 +0545

    styled cart-page

--- coffee-shop/src/components/Cart.jsx
+++ coffee-shop/src/components/Cart.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 import { IoIosArrowForward } from "react-icons/io";
 import { RxCross2 } from "react-icons/rx";
+import { FaCaretUp } from "react-icons/fa";
+import { FaCaretDown } from "react-icons/fa";
 
 const Cart = ({ cart, setCart }) => {
   const [carts, setCarts] = useState([]);
@@ -38,7 +40,7 @@ const Cart = ({ cart, setCart }) => {
         <table className='cart-table'>
             <thead>
                 <tr>
-                    <th className='table-product-name'>product name</th>
+                    <th className='table-head-name'>product name</th>
                     <th>price</th>
                     <th>quantity</th>
                     <th>total</th>
@@ -49,37 +51,24 @@ const Cart = ({ cart, setCart }) => {
                 {
                 carts.length > 0 ? 
                 carts.map((cartItem, cartIndex) => (
-                    <tr key={cartIndex} className='table-product'>
-                        <td>
-                            <img src={cartItem.image} alt='cartItem' width={200} />
-                            <p>{cartItem.title}</p>
-                        </td>
-                        <td><p>Rs.{cartItem.price * cartItem.quantity}</p></td>
-                        <td>
-                            <button onClick={() => handleDecrement(cartIndex)}>-</button>
-                            <p>{cartItem.quantity}</p>
-                            <button onClick={() => handleIncrement(cartIndex)}>+</button>
-                        </td>
-                        <td>..</td>
-                        <td><RxCross2 size={25} /></td>
-                        </tr>
+                    <tr key={cartIndex} className='table-tr'>
+                      <td className='table-product'>
+                          <img src={cartItem.image} alt='cartItem' width={200} />
+                          <p>{cartItem.title}</p>
+                      </td>
+                      <td><p>Rs.{cartItem.price}</p></td>
+                      <td className='table-quantity'>
+                          <button onClick={() => handleIncrement(cartIndex)}><FaCaretUp size={20} /></button>
+                          <p>{cartItem.quantity}</p>
+                          <button onClick={() => handleDecrement(cartIndex)}><FaCaretDown size={20} /></button>
+                      </td>
+                      <td>Rs.{cartItem.price * cartItem.quantity}</td>
+                      <td><RxCross2 size={25} /></td>
+                    </tr>
                     )) : <h3>Your cart is empty</h3>
                 }
             </tbody>
         </table>
-      {/* {
-        carts.length > 0 ? 
-            carts.map((cartItem, cartIndex) => (
-                <div key={cartIndex}>
-                    <img src={cartItem.image} alt='cartItem' width={200} />
-                    <p>{cartItem.title}</p>
-                    <button onClick={() => handleDecrement(cartIndex)}>-</button>
-                    <p>{cartItem.quantity}</p>
-                    <button onClick={() => handleIncrement(cartIndex)}>+</button>
-                    <p>Rs.{cartItem.price * cartItem.quantity}</p>
-                </div>
-            )) : <h3>Your cart is empty</h3>
-      } */}
 
       <p>Items: <span>{carts.reduce((total, item) => total + item.quantity, 0)}</span></p>
       <p>Total Amount: <span>{carts.reduce((total, item) => total + (item.price * item.quantity), 0)}</span></p>
